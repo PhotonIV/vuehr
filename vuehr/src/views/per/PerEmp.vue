aa633bace6dda11d04291a510d8f9b9b2ca652cd
Photon <158470926@qq.com>
2022-07-20 10:48:07 +0800

    test

--- vuehr/src/views/per/PerEmp.vue
+++ vuehr/src/views/per/PerEmp.vue
@@ -460,7 +460,7 @@
                         <el-col :span="6">
                             <el-form-item label="工号:" prop="workID">
                                 <el-input size="mini" style="width: 150px" prefix-icon="el-icon-edit"
-                                          v-model="emp.workID" placeholder="工号" disabled></el-input>
+                                          v-model="emp.workID" placeholder="工号" readonly></el-input>
                             </el-form-item>
                         </el-col>
                         <el-col :span="5">
